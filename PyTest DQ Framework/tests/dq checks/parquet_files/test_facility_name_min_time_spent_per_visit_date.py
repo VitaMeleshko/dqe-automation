d361b37f8c7dd6d6526f3238bcc71e87a84d9fae
Vita Meleshko <vita_meleshko@epam.com>
2026-04-19 23:55:10 +0300

    Add to testcheck_data_full_data_set

--- PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -55,9 +55,9 @@ def test_check_count(source_data, target_data, data_quality_library):
 
 @pytest.mark.parquet_data
 @pytest.mark.facility_name_min_time_spent_per_visit_date
-def test_check_data_completeness(source_data, target_data, data_quality_library):
+def check_data_full_data_set(source_data, target_data, data_quality_library):
     """Validate that all data from source is present in target"""
-    data_quality_library.check_data_completeness(source_data, target_data)
+    data_quality_library.check_data_full_data_set(source_data, target_data)
 
 
 @pytest.mark.parquet_data
